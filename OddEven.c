02a81bcdcc1e74b235a65b204bccbe196cf5dc58
adhikarilaxman <90101856+adhikarilaxman@users.noreply.github.com>
2024-01-06 23:49:42 +0530

    Add files via upload

---
+++ OddEven.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+int main(){
+    int number;
+        printf("Enter The Number:\n");
+        scanf("%d",&number);
+
+        if(number%2==0){
+            printf("The Given No. is Even\n");
+        }
+        else{
+            printf("The Given No. is odd\n");
+        }
+
+        return 0;
+    }
+
+
+
+
